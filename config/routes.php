c78fe34a534e5ce781e698ff0e3308f15f317e44
MP奥井 <okui@motionpicture.jp>
2018-08-05 14:26:42 +0900

    ログインページ作成

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,9 @@
  * @author Atsushi Okui <okui@motionpicture.jp>
  */
 
+use Cinemasunshine\PortalAdmin\Controller\AuthController;
 use Cinemasunshine\PortalAdmin\Controller\IndexController;
+
+$app->get('/login', AuthController::class . ':login')->setName('login');
 
 $app->get('/', IndexController::class . ':index')->setName('homepage');
---
+++ src/Controller/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AuthController.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Cinemasunshine\PortalAdmin\Controller;
+
+/**
+ * Auth controller class
+ */
+class AuthController extends BaseController
+{
+    /**
+     * login action
+     * 
+     * @param \Slim\Http\Request  $request
+     * @param \Slim\Http\Response $response
+     * @param array               $args
+     * @return \Slim\Http\Response
+     */
+    public function executeLogin($request, $response, $args)
+    {
+    }
+}
