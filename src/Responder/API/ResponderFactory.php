28f0830b09fc1173e46bfe925b7a4833fc85aea2
Atsushi Okui <okui@motionpicture.jp>
2019-09-05 16:34:00 +0900

    ResponderFactoryを追加
    phpstanのignoreErrorsも削除

---
+++ src/Responder/API/ResponderFactory.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ResponderFactory.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Cinemasunshine\PortalAdmin\Responder\API;
+
+/**
+ * Responder factory class
+ */
+class ResponderFactory
+{
+    /**
+     * factory
+     *
+     * @param string $name
+     * @return BaseResponder
+     */
+    public static function factory(string $name): BaseResponder
+    {
+        $class = __NAMESPACE__ . '\\' . ucfirst($name) . 'Responder';
+
+        return new $class();
+    }
+}
