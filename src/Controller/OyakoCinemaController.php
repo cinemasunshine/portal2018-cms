29e30fc2b49856500e5e755b5c9705ff9e730c52
Atsushi Okui <okui@motionpicture.jp>
2019-12-03 17:56:17 +0900

    おやこシネマ機能はマスター、本部アカウントだけ利用可能とする SASAKI-527

--- src/Controller/OyakoCinemaController.php
+++ src/Controller/OyakoCinemaController.php
@@ -7,11 +7,27 @@
 
 namespace Cinemasunshine\PortalAdmin\Controller;
 
+use Cinemasunshine\PortalAdmin\Exception\ForbiddenException;
+
 /**
  * OyakoCinema controller
  */
 class OyakoCinemaController extends BaseController
 {
+    /**
+     * {@inheritDoc}
+     */
+    protected function preExecute($request, $response): void
+    {
+        $user = $this->auth->getUser();
+
+        if ($user->isTheater()) {
+            throw new ForbiddenException();
+        }
+
+        parent::preExecute($request, $response);
+    }
+
     /**
      * index action
      *
